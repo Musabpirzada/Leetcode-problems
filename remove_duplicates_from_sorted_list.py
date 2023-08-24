50c502075493dfd67e0e532110391a6d7fa097f7
Syed Musab Pirzada <musabpirzada1@gmail.com>
2023-08-24 07:19:48 +0500

    Add files via upload

--- remove_duplicates_from_sorted_list.py
+++ remove_duplicates_from_sorted_list.py
@@ -21,3 +21,23 @@ class Solution:
                     prev = current
                 current = current.next
             return head
+
+#With Much Less memory useage.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+
+        while current is not None and current.next is not None:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
